9ce09154380eee1cb082c13ddb3ea224d315f704
Brenda Kiptim <kiptim54@gmail.com>
2018-03-17 05:28:42 +0300

    add initial setup

---
+++ Documents/Personal_Projects/Quoteapp/src/app/forms/forms.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote'
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './forms.component.html',
+  styleUrls: ['./forms.component.css']
+})
+export class FormsComponent implements OnInit {
+  Quote:string[];
+  addQuote(userName,quotes,author){
+    console.log(userName,quotes,author)
+    return false;
+  }
+  
+    
+
+  ngOnInit(){
+    console.log("running");
+    this.Quote.unshift(userName,quotes,author);
+    
+    
+
+}
+
+}
---
+++ Documents/Personal_Projects/Quoteapp/src/app/quote.ts
@@ -0,0 +1,4 @@
+export class Quote {
+    constructor (public username:string, public quote:string, public author:string){}
+ }
+ 
